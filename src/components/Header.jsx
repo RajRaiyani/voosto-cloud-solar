402086f33c496b5cf2263bbb84fba12007555a19
rajraiyani <rpraiyani7@gmail.com>
2026-06-03 16:30:12 +0530

    removed logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,41 +33,6 @@ export default function Header() {
         <div className="header-container">
           {/* Logo */}
           <Link href="/" className="logo-link">
-            <svg
-              className="logo-icon"
-              width="36"
-              height="36"
-              viewBox="0 0 36 36"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle cx="18" cy="18" r="16" fill="url(#logo-grad)" />
-              <path
-                d="M18 8C12.48 8 8 12.48 8 18C8 19.84 8.5 21.57 9.38 23.05L14 14.5L18.5 22L21 17.5L26.62 23.05C27.5 21.57 28 19.84 28 18C28 12.48 23.52 8 18 8Z"
-                fill="#060C17"
-              />
-              <path
-                d="M18 13C15.24 13 13 15.24 13 18C13 20.76 15.24 23 18 23C20.76 23 23 20.76 23 18C23 15.24 20.76 13 18 13ZM18 21.5C16.07 21.5 14.5 19.93 14.5 18C14.5 16.07 16.07 14.5 18 14.5C19.93 14.5 21.5 16.07 21.5 18C21.5 19.93 19.93 21.5 18 21.5Z"
-                fill="#00E699"
-              />
-              <path
-                d="M20.5 18C20.5 19.38 19.38 20.5 18 20.5C16.62 20.5 15.5 19.38 15.5 18C15.5 16.62 16.62 15.5 18 15.5C19.38 15.5 20.5 16.62 20.5 18Z"
-                fill="#00D2FF"
-              />
-              <defs>
-                <linearGradient
-                  id="logo-grad"
-                  x1="2"
-                  y1="2"
-                  x2="34"
-                  y2="34"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stopColor="#00E699" />
-                  <stop offset="1" stopColor="#00D2FF" />
-                </linearGradient>
-              </defs>
-            </svg>
             <span className="logo-text">
               cloud <span className="logo-highlight">solar</span>
             </span>
